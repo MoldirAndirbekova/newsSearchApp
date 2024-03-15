7002ff545d35fc94e1e16d37be88dd617ab0b050
Moldir <moldir.andirbekova@mail.ru>
2024-03-15 22:01:57 +0600

    Created NewsDatabase.

--- database/src/main/java/com/example/news/database/NewsDatabase.kt
+++ database/src/main/java/com/example/news/database/NewsDatabase.kt
@@ -1,4 +1,20 @@
 package com.example.news.database
 
-class NewsDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.news.database.dao.ArticleDao
+import com.example.news.database.models.ArticleDBO
+
+@Database(entities = [ArticleDBO::class], version = 1)
+abstract class NewsDatabase: RoomDatabase() {
+    abstract fun articleDao(): ArticleDao
+}
+
+fun NewsDatabase(applicationContext: Context): NewsDatabase {
+    return Room.databaseBuilder(
+        checkNotNull(applicationContext.applicationContext),
+        NewsDatabase::class.java, "news")
+        .build()
 }
