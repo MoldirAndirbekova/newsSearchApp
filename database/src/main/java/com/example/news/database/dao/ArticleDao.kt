fbfc7544897eb2f5d91d205b98259e26aed54593
Moldir <moldir.andirbekova@mail.ru>
2024-03-15 21:52:28 +0600

    Added queries in ArticleDao

---
+++ database/src/main/java/com/example/news/database/dao/ArticleDao.kt
@@ -0,0 +1,21 @@
+package com.example.news.database.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.news.database.models.ArticleDBO
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ArticleDao {
+    @Query("SELECT * FROM articles")
+    suspend fun getAll(): Flow<List<ArticleDBO>>
+    @Insert
+    suspend fun insert(articles: List<ArticleDBO>)
+    @Delete
+    suspend fun remove(articles: List<ArticleDBO>)
+
+    @Query("DELETE FROM articles")
+    suspend fun clean()
+}
