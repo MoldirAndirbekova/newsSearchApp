1103f980a8d044e4536abba081eefccd5409a555
Moldir <moldir.andirbekova@mail.ru>
2024-03-15 20:56:19 +0600

    Adding Composable dependencies in features module.

--- features/news-main/build.gradle.kts
+++ features/news-main/build.gradle.kts
@@ -39,6 +39,7 @@ android {
 }
 
 dependencies {
+
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
@@ -49,4 +50,8 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
 }
